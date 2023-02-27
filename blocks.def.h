a3cb2dfc9a62c92298c75af7505b989505c7016b
j0giwa <jonasschwin20022@gmx.de>
2023-02-27 15:04:51 +0100

    Updated the flielpath for the barscrpits

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"\x01", "echo Disp",	0,		0},
-	{"\x02", "echo Mem: $(free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g)",	10,		0},
-	{"\x03", "$HOME/.scripts/networkstatus.sh",	    30,		0},
-	{"\x04", "$HOME/.scripts/battery.sh",			60,		0},
-	{"\x05", "date '+%I:%M'",					    60,		0},
+	{"‌", "", 0, 0}, // USELESS - just decoration 
+	{"\x01", "wm-mpd", 5, 0},
+	{"\x02", "wm-memorystatus", 10, 0},
+	{"\x03", "wm-networkstatus", 15, 0},
+	{"\x04", "wm-battery", 60, 0},
+	{"\x05", "wm-clock", 60, 0},
+	{"‌", "", 0, 0}, // USELESS - just decoration 
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
